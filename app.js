7b3b391b9aa01c6c92609fc7bea728ebbf9fb1ab
Lucas Macias <lucasjmacias@gmail.com>
2023-11-13 11:37:24 -0300

    Fin Mission #4

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 import express from "express";
 import path from "path";
 import dotenv from "dotenv";
+import mainRoutes from "./src/routes/mainRoutes.js";
+import shopRoutes from "./src/routes/shopRoutes.js";
+import adminRoutes from "./src/routes/adminRoutes.js";
+import authRoutes from "./src/routes/authRoutes.js";
 
 dotenv.config();
 
@@ -10,6 +14,11 @@ const root = path.resolve();
 const PORT = process.env.PORT;
 
 app.use(express.static("public"));
+
+app.use("/", mainRoutes);
+app.use("/", shopRoutes);
+app.use("/", adminRoutes);
+app.use("/", authRoutes);
 
 // app.get("/home", (req, res) => {
 //     res.sendFile(path.join(root, "public", "index.html"));
